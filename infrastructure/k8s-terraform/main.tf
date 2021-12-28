060bd382d8cbbea7b4eea20e77b01b25fb74a3fc
james-clarusway <james@clarusway.com>
2021-12-28 19:49:47 +0300

    nightly-504

--- infrastructure/k8s-terraform/main.tf
+++ infrastructure/k8s-terraform/main.tf
@@ -192,8 +192,8 @@ output worker-1-ip {
   description = "public ip of the worker-1"
 }
 
-# output worker-2-ip {
-#   value       = aws_instance.worker-2.public_ip
-#   sensitive   = false
-#   description = "public ip of the worker-2"
-# }
+output worker-2-ip {
+  value       = aws_instance.worker-2.public_ip
+  sensitive   = false
+  description = "public ip of the worker-2"
+}
