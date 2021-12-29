f7b0da095ec3031115f0466c51d600069b34796d
james-clarusway <james@clarusway.com>
2021-12-29 14:11:52 +0300

    29dec1412

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -75,6 +75,12 @@ resource "aws_security_group" "matt-kube-master-sg" {
     to_port = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
+    ingress {
+    protocol = "tcp"
+    from_port = 30001
+    to_port = 30001
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     protocol = "tcp"
     from_port = 6443
