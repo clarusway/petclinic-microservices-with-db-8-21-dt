86ede2494fcf265672924012713bf85ae04279e4
james-clarusway <james@clarusway.com>
2022-01-03 21:55:24 +0300

    weekly-qa

--- ansible/scripts/deploy_app_on_qa_environment.sh
+++ ansible/scripts/deploy_app_on_qa_environment.sh
@@ -1,5 +1,5 @@
 PATH="$PATH:/usr/local/bin"
 APP_NAME="petclinic"
-sed -i "s/APP_STACK_NAME/${APP_STACK_NAME}/" ./ansible/inventory/qa_stack_dynamic_inventory_aws_ec2.yaml
-envsubst < docker-compose-swarm-qa.yml > docker-compose-swarm-qa-tagged.yml
-ansible-playbook -i ./ansible/inventory/qa_stack_dynamic_inventory_aws_ec2.yaml -b --extra-vars "workspace=${WORKSPACE} app_name=${APP_NAME} aws_region=${AWS_REGION} ecr_registry=${ECR_REGISTRY}" ./ansible/playbooks/pb_deploy_app_on_qa_environment.yaml
+envsubst < k8s/qa/kustomization-template.yml > k8s/qa/kustomization.yml
+envsubst < ansible/playbooks/qa-petclinic-deploy-template > ansible/playbooks/qa-petclinic-deploy.yaml
+ansible-playbook -i ${WORKSPACE}/ansible/inventory/qa_stack_dynamic_inventory_aws_ec2.yaml ${WORKSPACE}/ansible/playbooks/qa-petclinic-deploy.yaml
