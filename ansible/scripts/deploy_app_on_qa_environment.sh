1fa209c20ad5a752d28281bf956647d3c21db9cd
james-clarusway <james@clarusway.com>
2022-01-03 22:23:08 +0300

    weekly-qa

--- ansible/scripts/deploy_app_on_qa_environment.sh
+++ ansible/scripts/deploy_app_on_qa_environment.sh
@@ -1,5 +1,5 @@
 PATH="$PATH:/usr/local/bin"
 APP_NAME="petclinic"
-envsubst < k8s/qa/kustomization-template.yml > k8s/qa/kustomization.yml
+envsubst < k8s/base/kustomization-template.yml > k8s/qa/kustomization.yml
 envsubst < ansible/playbooks/qa-petclinic-deploy-template > ansible/playbooks/qa-petclinic-deploy.yaml
 ansible-playbook -i ${WORKSPACE}/ansible/inventory/qa_stack_dynamic_inventory_aws_ec2.yaml ${WORKSPACE}/ansible/playbooks/qa-petclinic-deploy.yaml
